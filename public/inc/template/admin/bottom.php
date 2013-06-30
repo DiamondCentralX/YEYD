0ec18c00f3e65e5dd18f0d9d1e1f28e2f205f16d
erty5000 <me@awesome>
2013-07-01 00:27:18 +0300

    v0.01.3 Admin panel fixes

--- public/inc/template/admin/bottom.php
+++ public/inc/template/admin/bottom.php
@@ -1,5 +1,5 @@
 		<div class="footer">
-			&copy; <?php echo $SITE['copyright_from'] . ' - ' . $SITE['copyright_to'] . ' ' . $SITE['copyright_owner']; ?>. All rights reserved.
+			Powered by <?php echo '<a target="_blank" href="' . $PROJECT['web'] . '">' . $PROJECT['title'] . ' v' . $PROJECT['version'] . '</a>'; ?>
 		</div>
 	</body>
 </html>
